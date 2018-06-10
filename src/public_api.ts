4010c2a7e89d520d5223ab839cb41bb0aafa303e
iwe7 <admin@meepo.com.cn>
2018-06-11 07:31:13 +0800

    ssave

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of iwe7-on-press
- */
-
-export * from './lib/iwe7-on-press.service';
-export * from './lib/iwe7-on-press.component';
 export * from './lib/iwe7-on-press.module';
